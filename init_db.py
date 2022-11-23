aaaf6bcd6b04324cd7a22b56b851593d1d7f6196
3st3ban3 <esteban.pattin.bas06@gmail.com>
2022-11-23 10:32:21 +0100

    better db schema

--- init_db.py
+++ init_db.py
@@ -3,7 +3,7 @@ import sqlite3
 conn = sqlite3.connect('repertoire.db')
 cur = conn.cursor()
 
-cur.execute('CREATE TABLE IF NOT EXISTS "CONTACT"("id" INT, "first_name" TEXT, "last_name" TEXT, "tel" TEXT, PRIMARY KEY("id"))')
+cur.execute('CREATE TABLE IF NOT EXISTS "CONTACT"("id" INTEGER PRIMARY KEY AUTOINCREMENT, "first_name" TEXT, "last_name" TEXT, "tel" TEXT)')
 
 conn.commit()
 
